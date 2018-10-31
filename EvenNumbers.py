b3abbb591d2c65426bd26251b8c5cf4cb083d1e7
Sreedevi179 <42407419+Sreedevi179@users.noreply.github.com>
2018-10-31 19:31:09 +0000

    Delete EvenNumbers.py

--- EvenNumbers.py
+++
@@ -1,9 +0,0 @@
-#function to find all the even numbers till the given range (including the number of the range)
-
-def get_even_numbers(number):
-	if number < 0 : return
-	even = [x for x in range(number+1) if x % 2 == 0]
-	return even
-
-ans = get_even_numbers(11)
-print (ans)
